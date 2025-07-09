b2d1aaae819c9809db904093de50446a37d6c02a
gracecleaners <okumuoscar1712@gmail.com>
2025-07-09 12:39:46 +0300

    splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_customer/constants/colors.dart';
+import 'package:food_delivery_customer/views/auth/login.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,12 +11,28 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    triggerNewScreen();
+    super.initState();
+  }
+
+  void triggerNewScreen() async {
+    await Future.delayed(Duration(seconds: 4));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+
+  @override
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: TColor.primary,
       body: Center(
-        child: Image.asset("assets/logo.png", width: media.width*0.7, fit: BoxFit.fill,),
+        child: Image.asset(
+          "assets/logo.png",
+          width: media.width * 0.7,
+          fit: BoxFit.fill,
+        ),
       ),
     );
   }
